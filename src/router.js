da36850cc01e7a302dd68d8c968646ea0434a60c
Bernard Gabon <hewbertgabon@gmail.com>
2021-06-20 06:52:59 +0800

    route base

--- src/router.js
+++ src/router.js
@@ -11,7 +11,7 @@ const routes = [
   ];
 
 const router = new VueRouter({
-    //base:'/',
+    base:'/GabsVuetifyNoCLINodeWebpack',
     mode: 'history', // real path
     routes, // short for `routes: routes`
   });
