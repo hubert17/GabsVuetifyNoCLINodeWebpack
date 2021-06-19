2864c5ecaee0ba14110325a3051c98ac6e746f4a
Bernard Gabon <hewbertgabon@gmail.com>
2021-06-20 06:22:22 +0800

    router no base

--- src/router.js
+++ src/router.js
@@ -11,7 +11,7 @@ const routes = [
   ];
 
 const router = new VueRouter({
-    base: window.location.pathName,
+    //base:'/',
     mode: 'history', // real path
     routes, // short for `routes: routes`
   });
