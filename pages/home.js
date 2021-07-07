7c6431bfe7c58e3f4ba2d4b3190be2ac4e4f4fd4
Bernard Gabon <hewbertgabon@gmail.com>
2021-07-07 11:51:11 +0800

    v-main sroll improvement

--- pages/home.js
+++ pages/home.js
@@ -9,18 +9,16 @@ export default {
 
     template: /*html*/ `
 
-<v-main>
-    <v-container>
-        <h1>
-            {{ title }}
-        </h1>
-        <v-row>
-            <v-col>
-                <v-img src="https://www.atlasandboots.com/wp-content/uploads/2019/05/ama-dablam2-most-beautiful-mountains-in-the-world.jpg"></v-img>
-            </v-col>
-        </v-row>
-    </v-container>
-</v-main>
+<v-container>
+    <h1>
+        {{ title }}
+    </h1>
+    <v-row>
+        <v-col>
+            <v-img src="https://www.atlasandboots.com/wp-content/uploads/2019/05/ama-dablam2-most-beautiful-mountains-in-the-world.jpg"></v-img>
+        </v-col>
+    </v-row>
+</v-container>
 
     `,
   };
